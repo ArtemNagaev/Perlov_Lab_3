0598ef4285f75904085edb964fcab1f4f049b8ad
ArtemNagaev <31846535+ArtemNagaev@users.noreply.github.com>
2018-10-30 20:08:11 +0500

    First XLSX

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,8 +5,24 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.streaming.SXSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.commons.compress.archivers.zip.ZipFile;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main extends Application {
+    
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -18,6 +34,28 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+        XSSFWorkbook workbook = null;
+        try {
+            workbook = new XSSFWorkbook(new FileInputStream("src/data/lab3_data.xlsx"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        XSSFSheet sheet = workbook.getSheet("Лист1");
+
+        ArrayList<Double> data = new ArrayList<Double>();
+
+        for (Row row: sheet) {
+            Cell cell = row.getCell(0);
+
+            data.add(cell.getNumericCellValue());
+        }
+
+
+        for (Double number:data
+             ) {
+            System.out.print(number + " ");
+
+        }
         launch(args);
     }
 }
