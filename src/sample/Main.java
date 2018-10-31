557c8541f084f294f26b56aea91d631d44062ae4
ArtemNagaev <31846535+ArtemNagaev@users.noreply.github.com>
2018-10-31 21:10:50 +0500

    Second SeasonComp

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -21,8 +21,41 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 
+
+
 public class Main extends Application {
-    
+
+    static  public Double sum(ArrayList<Double> inputList){
+        double sum = 0.0;
+        for (int i = 0; i < inputList.size(); i++){
+            sum+=inputList.get(i);
+        }
+        return sum;
+    }
+
+    static public ArrayList<Double> MovingAverage (ArrayList<Double> inputList, Integer window){
+        ArrayList<Double> outputList = new ArrayList<Double>();
+
+        for (int i = 0; i<=inputList.size() - window; i++){
+            Double tmpNum = 0.0;
+            for (int j=0 ; j < window; j++){
+                tmpNum+=inputList.get(i+j);
+            }
+            outputList.add(tmpNum/window);
+        }
+
+        return outputList;
+    }
+
+    static public double Average (ArrayList<Double> inputList){
+        double sum = 0;
+
+        for (double number:inputList
+             ) {
+            sum+=number;
+        }
+        return sum/inputList.size();
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -33,7 +66,9 @@ public class Main extends Application {
     }
 
 
+
     public static void main(String[] args) {
+
         XSSFWorkbook workbook = null;
         try {
             workbook = new XSSFWorkbook(new FileInputStream("src/data/lab3_data.xlsx"));
@@ -50,8 +85,80 @@ public class Main extends Application {
             data.add(cell.getNumericCellValue());
         }
 
+        //Скользящее среднее
+        ArrayList<Double> dataMA = new ArrayList<Double>();
+        dataMA = MovingAverage(data,12);
 
-        for (Double number:data
+        //Скользящее среднее центрированное
+        ArrayList<Double> dataMACentric = new ArrayList<Double>();
+        dataMACentric = MovingAverage(dataMA,2);
+
+        //Сглаженное скользящее среднее (Сезонная компонента)
+        ArrayList<Double> dataMAmean = new ArrayList<Double>();
+        for (int i = 6; i < data.size()-6; i++){
+            dataMAmean.add(data.get(i)/dataMACentric.get(i-6));
+        }
+
+        //Считаем среднюю сезонную компоненту
+
+        //Добавляем нулю в начало и конец для деления по месяцам
+        ArrayList<Double> dataMAmeanFixed = new ArrayList<Double>();
+        for (int i = 0; i < 6; i++){
+            dataMAmeanFixed.add(0.0);
+        }
+
+        for (int i = 0; i < dataMAmean.size(); i++){
+            dataMAmeanFixed.add(dataMAmean.get(i));
+        }
+
+        for (int i = 0; i < 6; i++){
+            dataMAmeanFixed.add(0.0);
+        }
+
+
+        //Записываем компоненты по месяцам
+        ArrayList<ArrayList<Double>> dataMAmeanMonth = new ArrayList<ArrayList<Double>>();
+
+        for (int i = 0; i<12; i++){
+            dataMAmeanMonth.add(new ArrayList<Double>());
+            for (int j = 0; j<dataMAmeanFixed.size(); j+=12){
+                dataMAmeanMonth.get(i).add(dataMAmeanFixed.get(i+j));
+            }
+        }
+
+
+        ArrayList<Double>  seasonComp = new ArrayList<Double>();
+
+        //убираем нули
+        for (int i = 0; i < dataMAmeanMonth.size(); i++){
+            dataMAmeanMonth.get(i).remove(0.0);
+        }
+
+        //Считаем среднюю сезонную компоненту
+        for (int i = 0; i < dataMAmeanMonth.size(); i++){
+            seasonComp.add(Average(dataMAmeanMonth.get(i)));
+        }
+
+        //Считаем корректирующий коэффициент
+        Double coef = seasonComp.size()/ sum(seasonComp);
+
+        //Считаем скорректированную сезонную компоненту
+        ArrayList<Double>  seasonCompCorr = new ArrayList<Double>();
+
+        for (int i = 0; i < seasonComp.size(); i++){
+            seasonCompCorr.add(seasonComp.get(i)*coef);
+        }
+
+
+        //считаем тренд
+        Double n = Double.valueOf(data.size());
+        Double SumXi = sum(data);
+
+        //находим значение без сезонной компоненты
+
+        //Double SumYi = sum()
+
+        for (Double number:seasonComp
              ) {
             System.out.print(number + " ");
 
